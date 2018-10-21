c1c5c6579f3e8ac0d5372863e59420bfb733e5c5
artyom-yurin <artem_yrin@mail.ru>
2018-10-21 15:52:19 +0300

    Added Kutta method

--- src/ImprovedEulerMethod.java
+++ src/ImprovedEulerMethod.java
@@ -1,14 +1,13 @@
 public class ImprovedEulerMethod extends EulerMethod{
-    
+
     ImprovedEulerMethod(int size, float x0, float y0, int xMax) {
         super(size, x0, y0, xMax);
     }
 
     @Override
     protected float CalculateY(int i) {
-        return super.AxisY[i - 1] +
-                super.getStep() / 2 *
-                        (getRhs().calculate(super.AxisX[i - 1], super.AxisY[i - 1]) +
-                                getRhs().calculate(super.AxisX[i], super.AxisY[i-1] + super.getStep() * super.getRhs().calculate(super.AxisX[i-1], super.AxisY[i-1])));
+        float k1 = getRhs().calculate(super.AxisX[i - 1], super.AxisY[i - 1]);
+        float k2 = getRhs().calculate(super.AxisX[i], super.AxisY[i-1] + super.getStep() * k1);
+        return super.AxisY[i - 1] + super.getStep() / 2 * (k1 + k2);
     }
 }
---
+++ src/KuttaMethod.java
@@ -0,0 +1,14 @@
+public class KuttaMethod extends EulerMethod {
+    KuttaMethod(int size, float x0, float y0, int xMax) {
+        super(size, x0, y0, xMax);
+    }
+
+    @Override
+    protected float CalculateY(int i) {
+        float k1 = getRhs().calculate(super.AxisX[i - 1], super.AxisY[i - 1]);;
+        float k2 = getRhs().calculate(super.AxisX[i - 1] + super.getStep() / 2, super.AxisY[i - 1] + super.getStep() / 2 * k1);
+        float k3 = getRhs().calculate(super.AxisX[i - 1] + super.getStep() / 2, super.AxisY[i - 1] + super.getStep() / 2 * k2);
+        float k4 = getRhs().calculate(super.AxisX[i], super.AxisY[i - 1] + super.getStep()* k3);
+        return super.AxisY[i-1] + super.getStep() / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+    }
+}
